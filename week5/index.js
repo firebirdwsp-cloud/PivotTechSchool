d2d1ac037ca488337759d1b7e6d01949e8664146
Jon Adam Gilliam <firebirdwsp@gmail.com>
2026-02-19 20:05:12 -0600

    week5feb19

--- week5/index.js
+++ week5/index.js
@@ -8,45 +8,45 @@ let name = "Leo R";
 let age = "35";
 let food = "Pizza";
 
-console.log ("my name is " + name + " and I am " + age +
-         " years old." + " and i love to eat " + food + "."); 
-          ;
+console.log("my name is " + name + " and I am " + age +
+    " years old." + " and i love to eat " + food + ".");
+;
 
 //functions
 
-function sayHello(){
+function sayHello() {
 
 
-console.log("hello");
-console.log("hello");
-console.log("hello");
-} 
+    console.log("hello");
+    console.log("hello");
+    console.log("hello");
+}
 sayHello();
 
-function addNumbers (a , b ) {
+function addNumbers(a, b) {
     console.log(a + b);
 
 }
-addNumbers(2, 3); 
+addNumbers(2, 3);
 addNumbers(11, 4);
-addNumbers(6, 1);  
+addNumbers(6, 1);
 
-function deleteNumbers (a , b ) {
-    console.log(a - b); 
+function deleteNumbers(a, b) {
+    console.log(a - b);
 }
-deleteNumbers(6, 8); 
+deleteNumbers(6, 8);
 deleteNumbers(9, 2);
 deleteNumbers(84, 56);
 
-function multiplyNumbers (a , b ) {
-    console.log(a * b); 
-}                           
+function multiplyNumbers(a, b) {
+    console.log(a * b);
+}
 multiplyNumbers(6, 8);
 multiplyNumbers(9, 2);
-multiplyNumbers(84, 56);        
-function divideNumbers (a , b ) {
-    console.log(a / b); 
-}                                   
+multiplyNumbers(84, 56);
+function divideNumbers(a, b) {
+    console.log(a / b);
+}
 divideNumbers(6, 8);
 divideNumbers(9, 2);
 divideNumbers(84, 56);
@@ -55,5 +55,28 @@ let favColor = "pink";
 favColor = "blue";
 console.log(favColor);
 favColor = "green";
+
+let score = 10
+const color = "red";
+score = 15;
+console.log(score);
+
+function submitName() {
+    const nameValue = document.getElementById("name").value;
+    alert("Name: " + nameValue);
+}
+
+function submitEmail() {
+    const emailValue = document.getElementById("email").value;
+    alert("Email: " + emailValue);
+}
+
+
+const myName = "Adam";
+const myEmail = "@adam.com";
+alert(myName + ".com");
+
+const myEmail = myName + "@gmail.com";
+alert(myEmail);
 
 
