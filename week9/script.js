91c274ca9d7898b3b8c0ec3af24bfc65d16986b9
Jon Adam Gilliam <firebirdwsp@gmail.com>
2026-03-17 18:40:16 -0500

    week9night2

--- week9/script.js
+++ week9/script.js
@@ -1,45 +1,18 @@
 function checkLogin() {
-  let username = document.getElementById("username").value;
+  console.log("Submit button clicked");
+
+  let email = document.getElementById("email").value;
   let password = document.getElementById("password").value;
   let message = document.getElementById("message");
 
-  let correctUsername = "admin";
+  let correctEmail = "admin@email.com";
   let correctPassword = "1234";
 
-  if (username === correctUsername && password === correctPassword) {
+  if (email === correctEmail && password === correctPassword) {
     message.textContent = "Login successful!";
     message.style.color = "green";
   } else {
-    message.textContent = "Incorrect username or password.";
+    message.textContent = "Incorrect email or password.";
     message.style.color = "red";
   }
-
-  function checkLogin() {
-  let username = document.getElementById("username").value;
-  let password = document.getElementById("password").value;
-  let message = document.getElementById("message");
-
-  console.log("Submit button was clicked");
-  console.log("Username entered:", username);
-  console.log("Password entered:", password);
-
-  let correctUsername = "admin";
-  let correctPassword = "1234";
-
-  if (username === correctUsername && password === correctPassword) {
-    message.textContent = "Login successful!";
-    message.style.color = "green";
-    console.log("Login successful");
-  } else {
-    message.textContent = "Incorrect username or password.";
-    message.style.color = "red";
-    console.log("Login failed");
-  }
-console.log("username:", username);
-console.log("password:", password); 
-
-
-
-   
-}
 }
