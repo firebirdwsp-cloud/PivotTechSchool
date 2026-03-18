cbbaa7abf514569623f9f9bb78c4448c257c7d6e
Jon Adam Gilliam <firebirdwsp@gmail.com>
2026-03-17 19:27:44 -0500

    wk9night2

--- week9/script.js
+++ week9/script.js
@@ -15,6 +15,27 @@ function checkLogin() {
     message.textContent = "Incorrect email or password.";
     message.style.color = "red";
   }
+function checkLogin() {
+  console.log("Submit button clicked");
+
+  let email = document.getElementById("email").value;
+  let password = document.getElementById("password").value;
+  let message = document.getElementById("message");
+
+  let correctEmail = "admin@email.com";
+  let correctPassword = "1234";
+
+  if (email === correctEmail && password === correctPassword) {
+    message.textContent = "Login successful!";
+    message.style.color = "green";
+
+    window.location.href = "profile.html";
+  } else {
+    message.textContent = "Incorrect email or password.";
+    message.style.color = "red";
+  }
+}
+
 }   
 
 //email is admin@email.com and pw is 1234
